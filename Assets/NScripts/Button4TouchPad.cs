35562f40f5ea398747fbb8a34e6a95933c38f369
nnna <nagidec.24a@gmail.com>
2024-11-12 23:47:22 +0900

    タッチパネルでのホールドノーツ機能を追加

--- Assets/NScripts/Button4TouchPad.cs
+++ Assets/NScripts/Button4TouchPad.cs
@@ -4,7 +4,6 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 /// <summary>
 /// タッチパッドに対応させるためのクラス
@@ -16,11 +15,17 @@ public class Button4TouchPad : MonoBehaviour
     private FlushLight _flushLight = null;
     private RayCastGenerator _raycastGenerator = null;
     private EventTrigger _eventTrigger = null;
+    private bool _isHolding = false;
 
     private void Start()
     {
         GetMyComponents();
         SetListeners();
+    }
+
+    private void Update()
+    {
+        if (_isHolding) OnPointerHold();
     }
 
     private void GetMyComponents()
@@ -44,15 +49,36 @@ public class Button4TouchPad : MonoBehaviour
 
     private void SetListeners()
     {
-        EventTrigger.Entry entry = new EventTrigger.Entry();
-        entry.eventID = EventTriggerType.PointerDown;
-        entry.callback.AddListener((data) => { PointerDown(); });
-        _eventTrigger.triggers.Add(entry);
+        EventTrigger.Entry pointerDownEntry = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerDown
+        };
+        pointerDownEntry.callback.AddListener((data) => { OnPointerDown((PointerEventData)data); });
+        _eventTrigger.triggers.Add(pointerDownEntry);
+
+        EventTrigger.Entry pointerUpEntry = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerUp
+        };
+        pointerUpEntry.callback.AddListener((data) => { OnPointerUp((PointerEventData)data); });
+        _eventTrigger.triggers.Add(pointerUpEntry);
     }
 
-    private void PointerDown()
+    private void OnPointerDown(PointerEventData data)
     {
         _raycastGenerator.ProcessKeyDown_RcGen();
         _flushLight.colorChange();
+        _isHolding = true;
+    }
+
+    private void OnPointerUp(PointerEventData data)
+    {
+        _raycastGenerator.ProcessKeyUp_RcGen();
+        _isHolding = false;
+    }
+
+    private void OnPointerHold()
+    {
+        _raycastGenerator.ProcessKeyHold_RcGen();
     }
 }
