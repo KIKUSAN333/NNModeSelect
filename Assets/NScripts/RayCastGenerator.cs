9eaad27ef2c8710d5163fa76d423f74a99571218
nnna <nagidec.24a@gmail.com>
2024-11-14 23:06:00 +0900

    ノーツを複数取得し，最近傍の狙ったノーツを処理するように変更

--- Assets/NScripts/RayCastGenerator.cs
+++ Assets/NScripts/RayCastGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RayCastGenerator : MonoBehaviour
@@ -10,10 +11,12 @@ public class RayCastGenerator : MonoBehaviour
 
     public AudioSource audioSource;
     [SerializeField] private AudioClip hitSound;
+    private AudioClip holdSound;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        holdSound  = (AudioClip)Resources.Load("SEs/TapSE_1");
     }
 
     void Update()
@@ -41,13 +44,18 @@ public class RayCastGenerator : MonoBehaviour
             }
         }
 
-        hitObject = ShootRaycastInXDirection();
-        if (hitObject == null) return;
+        List<GameObject> hitObjects = ShootRaycastInXDirection();
+        if (hitObjects.Count == 0) return;
 
-        notes = hitObject.GetComponent<Notes>();
-        if (notes == null) return;
-
-        notes.ProcessHitRaycast(Math.Abs(hitObject.transform.position.x - this.transform.position.x));
+        foreach (var h in hitObjects)
+        {
+            notes = h.GetComponent<Notes>();
+            if (notes != null)
+            {
+                notes.ProcessHitRaycast(Math.Abs(h.transform.position.x - this.transform.position.x));
+                break;
+            }
+        }
     }
 
     /// <summary>
@@ -63,7 +71,7 @@ public class RayCastGenerator : MonoBehaviour
             if (notes != null)
             {
                 notes.ProcessInputEvent();
-                audioSource.PlayOneShot(hitSound);
+                audioSource.PlayOneShot(holdSound);
                 return;
             }
         }
@@ -87,31 +95,32 @@ public class RayCastGenerator : MonoBehaviour
             }
         }
 
-        hitObject = ShootRaycastInXDirection();
-        if (hitObject == null) return;
+        List<GameObject> hitObjects = ShootRaycastInXDirection();
+        if (hitObjects.Count == 0) return;
 
-        notes = hitObject.GetComponent<HoldEndNotes>();
-        if (notes == null) return;
-
-        notes.ProcessInputEvent(Math.Abs(hitObject.transform.position.x - this.transform.position.x));
-        audioSource.PlayOneShot(hitSound);
+        foreach (var h in hitObjects)
+        {
+            notes = h.GetComponent<HoldEndNotes>();
+            if (notes != null)
+            {
+                notes.ProcessInputEvent(Math.Abs(h.transform.position.x - this.transform.position.x));
+                audioSource.PlayOneShot(hitSound);
+                break;
+            }
+        }
     }
 
-    private GameObject ShootRaycastInXDirection()
+    private List<GameObject> ShootRaycastInXDirection()
     {
         Vector3 rayOrigin = transform.position;
 
-        RaycastHit2D hitRight = Physics2D.Raycast(rayOrigin, Vector2.right, Mathf.Infinity, mask);
+        RaycastHit2D[] hitRight = Physics2D.RaycastAll(rayOrigin, Vector2.right, 5.0f, mask);
 
-        RaycastHit2D hitLeft = Physics2D.Raycast(rayOrigin, Vector2.left, Mathf.Infinity, mask);
+        RaycastHit2D[] hitLeft = Physics2D.RaycastAll(rayOrigin, Vector2.left, 5.0f, mask);
 
-        if (hitRight.collider == null && hitLeft.collider == null) return null;
-        if (hitRight.collider != null && hitLeft.collider == null) return hitRight.collider.gameObject;
-        if (hitRight.collider == null && hitLeft.collider != null) return hitLeft.collider.gameObject;
-
-        RaycastHit2D hit = Mathf.Min(hitLeft.distance, hitRight.distance) == hitLeft.distance ? hitLeft : hitRight;
-
-        return hit.collider.gameObject;
+        RaycastHit2D[] hits = hitRight.Concat(hitLeft).ToArray();
+        Array.Sort(hits, (x, y) => x.distance.CompareTo(y.distance));
+        return hits.Select(hit => hit.collider.gameObject).ToList();
     }
 
     private GameObject GetNotesOnLine()
